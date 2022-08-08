8ebaed114b5141536b660e00a93aadfd361036eb
manchott <manchott@naver.com>
2022-08-08 22:49:06 +0900

    0808

---
+++ 0808/counting_sort.py
@@ -0,0 +1,15 @@
+def Counting_sort(A, B, k):
+    # A: 입력 배열(1 to k)
+    # B: 정렬된 배열
+    # C: 카운트 배열
+    C = [0] * (k + 1)
+
+    for i in range(0, len(A)):
+        C[A[i]] += i
+
+    for i in range(1, len(C)):
+        C[i] += C[i-1]
+
+    for i in range(len(B) - 1, -1, -1):
+        C[A[i]] -= 1
+        B[C[A[i]]] = A[i]
