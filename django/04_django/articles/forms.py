cb161ea4d080e4cc5b8537e852f12a1375a4afd5
manchott <manchott@naver.com>
2022-09-06 23:25:26 +0900

    0906

--- django/04_django/articles/forms.py
+++ django/04_django/articles/forms.py
@@ -16,6 +16,32 @@ from .models import Article
 #     nation = forms.ChoiceField(choices=NATIONS_CHOICES, widget=forms.RadioSelect)
 
 class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-title form-control',
+                'placeholder': 'Enter the title',
+                # 유효성 검사와 관련 x
+                'maxlength': 10,
+            }
+        )
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'my-content form-control',
+                'placeholder': 'Enter the title',
+                'rows': 5,
+                'cols': 50,
+                
+            }
+        ),
+        error_messages={
+            'required' : 'Please enter the content'
+        }
+    )
 
     class Meta:
         model = Article
