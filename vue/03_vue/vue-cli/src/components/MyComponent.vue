44ca14e2bd37fe2a3c797e7cf0c27dfa8a6ab683
manchott <manchott@naver.com>
2022-11-02 17:50:17 +0900

    1102

---
+++ vue/03_vue/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="border">
+    <h1>싸피 이거는 내가 만든 새로운 컴포넌트다!</h1>
+    <MyComponentItem/>
+  </div>
+</template>
+
+<script>
+// 1. 불러오기
+import MyComponentItem from '@/components/MyComponentItem'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    // 2. 등록하기
+    MyComponentItem,
+  }
+}
+</script>
+
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
---
+++ vue/1102/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="border">
+    <h1>내가 만든 컴포넌트!</h1>
+    <MyComponentItem 
+      static-props="MyComponent에서 보낸 데이터"
+      :dynamic-props="dynamicProps"
+      @child-to-parent="parentGetEvent"
+      @child-input="getDynamicData"
+    />
+  </div>
+</template>
+
+<script>
+import MyComponentItem from '@/components/MyComponentItem'
+export default {
+  name:'MyComponent',
+  components: {
+    MyComponentItem,
+  },
+  data: function () {
+    return {
+      dynamicProps: '이건 동적인 데이터!!',
+    }
+  },
+  methods:{
+    parentGetEvent: function(childData) {
+      console.log(childData)
+      console.log('용돈 없어!!')
+    },
+    getDynamicData: function(childInputData) {
+      console.log(`사용자의 입력은 ${childInputData}입니다`)
+    }
+  }
+}
+</script>
+<style>
+  .border {
+    border: solid 1px black;
+  }
+</style>
