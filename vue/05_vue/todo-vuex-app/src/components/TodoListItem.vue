47bd6a1db8e7208ac55350623bb3ac47b4589e2a
angielxx <angielxx94@gmail.com>
2022-11-08 17:53:51 +0900

    1108

---
+++ vue/05_vue/todo-vuex-app/src/components/TodoListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span 
+      @click="updateTodoStatus"
+      :class="{ 'is-completed': todo.isCompleted }"
+    >{{ todo.title }}</span>
+    <button @click="deleteTodo">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoListItem",
+  props: {
+    todo: Object,
+  },
+  methods: {
+    deleteTodo(){
+      // console.log("press button")
+      this.$store.dispatch('deleteTodo', this.todo)
+      // this.$store.commit('DELETE_TODO', this.todo) 바로 mutations으로 가기 가능
+
+    },
+    updateTodoStatus() {
+      this.$store.dispatch('updateTodoStatus', this.todo)
+    }
+  }
+
+}
+</script>
+
+<style>
+  .is-completed {
+    text-decoration: line-through;
+  }
+</style>
